489803fa89ba6bc27355ca82d6b8c934b555a9d4
Jackoo2162 <35918848+Jackoo2162@users.noreply.github.com>
2018-02-04 13:13:00 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,103 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('message', message => {
+  if (message.content === 'niga isseoya sal su isseo every day, every night I can feel you') {
+   // message.reply('pong');
+    message.channel.sendMessage('neon neomu areumdawo nuneul ttel suga eopseo oh no');
+  }
+});
+
+
+bot.on('message', message => {
+  if (message.content === 'oh no') {
+   // message.reply('pong');
+    message.channel.sendMessage('kkoche pin gasigati jjillil geot gatado neol gatgo sipeo');
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === '') {
+   // message.reply('pong');
+    message.channel.sendMessage('');
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === '') {
+   // message.reply('pong');
+    message.channel.sendMessage('');
+  }
+});
+
+
+bot.on('message', message => {
+  if (message.content === '') {
+   // message.reply('pong');
+    message.channel.sendMessage('');
+  }
+});
+
+
+bot.on('message', message => {
+  if (message.content === '') {
+   // message.reply('pong');
+    message.channel.sendMessage('');
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === '') {
+   // message.reply('pong');
+    message.channel.sendMessage('');
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === '') {
+   // message.reply('pong');
+    message.channel.sendMessage('');
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === '') {
+   // message.reply('pong');
+    message.channel.sendMessage('');
+  }
+});
+
+bot.on('message', message => {
+  if (message.content === '') {
+   // message.reply('pong');
+    message.channel.sendMessage('');
+  }
+});
+
+
+bot.on("ready", () => {
+  bot.user.setPresence({game: {name:"DRINKING COFFEE WITH MINHYUK BOT"}})
+  console.log('I have jackoo: {SERVERNAME}')
+})
+
+
+bot.on('ready', ()=>{
+  console.log(`Logged in as ${bot.user.tag}!`);
+  voiceChannel = bot.guilds.find('name', 'DREAMENERG3T1CER').channels.find('name', 'KIHYUN');
+  voiceChannel.join().then(()=>{
+      console.log(`Connected to ${voiceChannel.name}!`);
+  });
+});
+
+// Create an event listener for new guild members
+bot.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'general');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
+
+bot.login('NDA3NDk3MDQ2NDY4MTk4NDAw.DVCgjA.kEoqVCuzGcckBvqn1q6x7auTHcw');
